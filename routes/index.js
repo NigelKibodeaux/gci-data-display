4829e95c2ceef6faae7e2ad29ec5b6b21a04fbcc
Nigel Kibodeaux <nigel@team.about.me>
2015-10-24 12:53:55 -0800

    implemented caching

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,46 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var scrape = require('../lib/scrape')
+
+var last_scrape = {
+    date: null,
+    data: null,
+}
+
+// helper to send response
+function respondWithCachedData(res) {
+    var table = last_scrape.data
+        .match(/<table id="internet-detail-usage">[\s\S]+?<\/table>/)[0]
+    var total = last_scrape.data
+        .match(/<span class="total">([^<]+)<\/span>/)[1]
+
+    res.render('index', {
+        table: table,
+        total: total,
+        last_scrape: last_scrape.date,
+    })
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log(last_scrape.date)
+    console.log(Date.now() - last_scrape.date)
+
+    // return cached data if it's less than one hour old
+    if ( last_scrape.data && (Date.now() - last_scrape.date < 1000*60*60) ) {
+        console.log('returning cached data')
+        return respondWithCachedData(res)
+    }
+
+    // else, scrape it and store it
     scrape(function(err, data) {
-    // require('fs').readFile('./lib/GCI MyUsage.html', 'utf8', function(err, data) {
         if (err) return next(err)
 
-        var table = data.match(/<table id="internet-detail-usage">[\s\S]+?<\/table>/)[0]
-        var total = data.match(/<span class="total">([^<]+)<\/span>/)[1]
+        last_scrape.date = new Date();
+        last_scrape.data = data;
 
-        res.render('index', { table: table, total: total })
+        respondWithCachedData(res)
     });
 });
 
